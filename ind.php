6e1123a90bae9850a9d5b36eaa0909af71a91c64
sxe2255 <sxe2255@gmail.com>
2022-01-06 11:34:08 +0300

    Add files via upload

--- ind.php
+++ ind.php
@@ -64,8 +64,8 @@ if($content){
   $contentSMassegeId = $contentSMassege['message_id'];
   $textForSend='';
     $textForSend = (string)$contentSMassegeId;
-    sendMessage($website, $params2);
-    //editMessage($website, $params);
+    //sendMessage($website, $params2);
+    editMessage($website, $params);
 }
 else{
   $textForSend = $content;
