2a97e63377e48b4cf85c934b6cf839a9ffbd6c2b
sxe2255 <sxe2255@gmail.com>
2022-03-01 16:44:58 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -82,7 +82,7 @@
 if($content){
 
   $params['message_id'] = $contentMessageId;
-  $params['text'] = $contentMessage;
+  $params['text'] = $contentData;
 
     sendMessage($website, $params);
     //sendMessageCont($website, $params);
