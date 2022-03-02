3e194c03c846aa7e2d99f0406b1f8d121813995b
sxe2255 <sxe2255@gmail.com>
2022-03-02 17:06:32 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -1,4 +1,6 @@
 <?php 
+  include 'keyboard.php';
+
   $content = file_get_contents("php://input");
   $update = json_decode($content, true);
 	$textForSend = "hi hi hi"; 
@@ -23,139 +25,7 @@
   $href='';
   $pArr=array('<p>','</p>','<br>');
   $prot = 111;
-    $keyboard = array(
-    array(
-      array('text'=>'Картридер',
-            'callback_data'=>"{'Картридер':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Изображение с камер (SMART)',
-            'callback_data'=>"{'Изображение с камер (SMART)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Свободное место VIDEO (SMART)',
-            'callback_data'=>"{'Свободное место VIDEO(SMART)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Факт записи камер (SMART)',
-            'callback_data'=>"{'Факт записи камер (SMART)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Очистка LOG-файлов',
-            'callback_data'=>"{'Очистка LOG-файлов':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Тест диспенсера',
-            'callback_data'=>"{'Тест диспенсера':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Тест чекового принтера',
-            'callback_data'=>"{'Тест чекового принтера':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Тестовая операция',
-            'callback_data'=>"{'Тестовая операция':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Изображение с камер (TRAL)',
-            'callback_data'=>"{'Изображение с камер (TRAL)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Состояние HDD (TRAL)',
-            'callback_data'=>"{'Состояние HDD (TRAL)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Факт записи камер (TRAL)',
-            'callback_data'=>"Факт записи камер (TRAL)':'false'}"
-      )
-    ),
-    array(
-      array('text'=>'Закончил!',
-            'callback_data'=>"{'Закончил!':'false'}"
-      )
-    )
-  );
-  $keyboard2 = array(
-    array(
-      array('text'=>'Картридер'."\xE2\x9C\x85",
-        //'url'=>$href,
-        'callback_data'=>"{'Картридер':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Изображение с камер (SMART)'."\xE2\x9C\x85",
-            'callback_data'=>"{'Изображение с камер (SMART)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Свободное место VIDEO (SMART)'."\xE2\x9C\x85",
-            'callback_data'=>"{'Свободное место VIDEO (SMART)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Факт записи камер (SMART)'."\xE2\x9C\x85",
-            'callback_data'=>"{'Факт записи камер (SMART)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Очистка LOG-файлов'."\xE2\x9C\x85",
-            'callback_data'=>"{'Очистка LOG-файлов':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Тест диспенсера'."\xE2\x9C\x85",
-            'callback_data'=>"{'Тест диспенсера':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Тест чекового принтера'."\xE2\x9C\x85",
-            'callback_data'=>"{'Тест чекового принтера':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Тестовая операция'."\xE2\x9C\x85",
-            'callback_data'=>"{'Тестовая операция':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Изображение с камер (TRAL)'."\xE2\x9C\x85",
-            'callback_data'=>"{'Изображение с камер (TRAL)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Состояние HDD (TRAL)'."\xE2\x9C\x85",
-            'callback_data'=>"{'Состояние HDD (TRAL)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Факт записи камер (TRAL)'."\xE2\x9C\x85",
-            'callback_data'=>"Факт записи камер (TRAL)':'true'}"
-      )
-    ),
-    array(
-      array('text'=>'Закончил!'."\xE2\x9C\x85",
-            'callback_data'=>"{'Закончил!':'true'}"
-      )
-    )
-  );
-  $keyboard3 = array(
-    array(
-       array('text'=>'Картридер'."\xE2\x9C\x85",
-        //'url'=>$href,
-        'callback_data'=>"{'text':".$prot.",'Картиридер':'false','mjmj':'123asdz'}"
-        )
-    )
-  );
+  
 
   $botToken="2129085674:AAG-NyC4bNJFEeUhy8ywWD0O-T2gfObm97I";
   $chatId = 91211691;
