dd280ba62c733641d7428ab83bac9b3c85228961
sxe2255 <sxe2255@gmail.com>
2022-03-02 12:42:48 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -32,34 +32,54 @@
     array(
       array('text'=>'Изображение с камер (SMART)',
             'callback_data'=>"{'Изображение с камер (SMART)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Свободное место VIDEO (SMART)',
             'callback_data'=>"{'Свободное место VIDEO(SMART)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Факт записи камер (SMART)',
             'callback_data'=>"{'Факт записи камер (SMART)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Очистка LOG-файлов',
             'callback_data'=>"{'Очистка LOG-файлов':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тест диспенсера',
             'callback_data'=>"{'Тест диспенсера':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тест чекового принтера',
             'callback_data'=>"{'Тест чекового принтера':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тестовая операция',
             'callback_data'=>"{'Тестовая операция':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Изображение с камер (TRAL)',
             'callback_data'=>"{'Изображение с камер (TRAL)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Состояние HDD (TRAL)',
             'callback_data'=>"{'Состояние HDD (TRAL)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Факт записи камер (TRAL)',
             'callback_data'=>"Факт записи камер (TRAL)':'false'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Закончил!',
             'callback_data'=>"{'Закончил!':'false'}"
       )
@@ -70,37 +90,59 @@
       array('text'=>'Картридер'."\xE2\x9C\x85",
         //'url'=>$href,
         'callback_data'=>"{'Картридер':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Изображение с камер (SMART)'."\xE2\x9C\x85",
             'callback_data'=>"{'Изображение с камер (SMART)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Свободное место на диске VIDEO более 100 ГБ (SMART)'."\xE2\x9C\x85",
             'callback_data'=>"{'Свободное место на диске VIDEO более 100 ГБ (SMART)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Факт записи камер (SMART)'."\xE2\x9C\x85",
             'callback_data'=>"{'Факт записи камер (SMART)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Очистка системного диска от старый LOG-файлов'."\xE2\x9C\x85",
             'callback_data'=>"{'Очистка системного диска от старый LOG-файлов':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тест диспенсера'."\xE2\x9C\x85",
             'callback_data'=>"{'Тест диспенсера':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тест чекового принтера'."\xE2\x9C\x85",
             'callback_data'=>"{'Тест чекового принтера':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Тестовая операция'."\xE2\x9C\x85",
             'callback_data'=>"{'Тестовая операция':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Изображение с камер (TRAL)'."\xE2\x9C\x85",
             'callback_data'=>"{'Изображение с камер (TRAL)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Состояние HDD (TRAL)'."\xE2\x9C\x85",
             'callback_data'=>"{'Состояние HDD (TRAL)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Факт записи камер (TRAL)'."\xE2\x9C\x85",
             'callback_data'=>"Факт записи камер (TRAL)':'true'}"
-      ),
+      )
+    ),
+    array(
       array('text'=>'Закончил!'."\xE2\x9C\x85",
             'callback_data'=>"{'Закончил!':'true'}"
       )
