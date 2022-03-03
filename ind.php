adac9927074f2bed83b8118f091b8d368788617d
sxe2255 <sxe2255@gmail.com>
2022-03-03 16:27:32 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -63,7 +63,7 @@
 if($content){
 
   $params['message_id'] = $contentMessageId;
-  $params2['text'] =''.json_encode($contentReplyMarkup);
+  $params2['text'] =''.$contentReplyMarkup['inline_keyboard'];
 
    sendMessage($website, $params2);
     //sendMessageCont($website, $params);
