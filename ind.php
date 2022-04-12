a4fb7f959fd2fe122862fa916c26a612c6f8c694
sxe2255 <sxe2255@gmail.com>
2022-04-12 17:05:23 +0300

    Update ind.php

--- ind.php
+++ ind.php
@@ -71,7 +71,7 @@ if($content){
 //     if($inlineKeyboardReplyMarkup[0] === $stringDataFromRequest){
 //       $params2['text'] =''.$stringDataFromRequest;
 //     }
-	  $params2['text'] =''.$contentReplyMarkup['inline_keyboard'][$x][0];
+	  $params2['text'] =''.$contentReplyMarkup['inline_keyboard'][$x][0]->text;
   }
   $inlineKeyboardReplyMarkup = $contentReplyMarkup['inline_keyboard'][2][0]->text;
 
